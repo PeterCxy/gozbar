287e71fb86ee57f9d39149d04a6f4321630bc2f2
Peter Cai <xqsx43cxy@126.com>
2015-10-14 14:41:31 +0000

    test qrcode
    Signed-off-by: Peter Cai <xqsx43cxy@126.com>

--- scanner_test.go
+++ scanner_test.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"os"
 	"image/png"
+	"image/jpeg"
+
+	//iconv "github.com/djimenez/iconv-go"
 )
 
 func TestBarcode(t *testing.T) {
@@ -12,6 +15,7 @@ func TestBarcode(t *testing.T) {
 
 	if err != nil {
 		t.Fail()
+		return
 	}
 
 	i, _ := png.Decode(f)
@@ -27,6 +31,33 @@ func TestBarcode(t *testing.T) {
 		fmt.Println(str)
 
 		if str != "9876543210128" {
+			t.Fail()
+		}
+	})
+}
+
+func TestQRCode(t *testing.T) {
+	f, err := os.Open("testdata/qr.jpg")
+
+	if err != nil {
+		t.Fail()
+		return
+	}
+
+	i, _ := jpeg.Decode(f)
+	img := FromImage(i)
+
+	s := NewScanner()
+	s.SetConfig(0, CFG_ENABLE, 1)
+	s.Scan(img)
+
+	img.First().Each(func(str string) {
+		// Charset decoding
+		//str, _ = iconv.ConvertString(str, "utf-8", "iso-8859-1")
+		//str, _ = iconv.ConvertString(str, "iso-8859-1", "utf-8")
+		fmt.Println(str)
+
+		if str != "ZBar big law good! ZBar大法好!" {
 			t.Fail()
 		}
 	})
